337b47acc909368dcee6222b2ddf6986f4f75f81
Forward <Noah.Forward@k202domain.com>
2024-05-17 10:19:54 -0230

    Added bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,47 +1,52 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import movieData from './movies.json'
 import { useState } from 'react';
 
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+
 function App() {
-  
   return (
-    <>
+    <Container>
       <NavBar></NavBar>
-    </>
+    </Container>
   );
 }
 
 function NavBar() {
   let [loginStatus, setLoginStatus] = useState("Login")
   return (
-    <>
-      <h1>Assignment 1</h1>
-      <button onClick={(evt) => {
+    <div class="text-center container bg-light p-3 my-3 border">
+      <h1 >Assignment 1</h1>
+      <Button variant="primary" onClick={(evt) => {
         loginStatus === "Login" ? setLoginStatus("Logout") : setLoginStatus("Login")
-      }}>{loginStatus}</button>
+      }}>{loginStatus}</Button>
+      <br></br>
+      <br></br>
       <MovieList movies={movieData.movies}></MovieList>
-    </>
+    </div>
   );
 }
 
 function MovieList({movies}) {
   return (
-    <>
+    <div class="text-start">
       <h2>Movie Ratings</h2>
       {movies.map( movie => <Movie movie={movie}></Movie>)}
-    </>
+    </div>
   );
 }
 
 function Movie({movie}) {
   return (
-    <>
-      <p>{movie.name} ({movie.year}): {movie.rating} / 5</p>
-    </>
+    <div class="card  bg-light mb-3" style={{width: 18 + "rem", margin: 10 + "px", padding: 10 + "px"}}>
+      <h5 class="card-title">{movie.name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">{movie.year}</h6>
+      <p class="card-text">⭐ {movie.rating} / 5 </p>
+    </div>
   );
   
 }
-
-
 
 export default App;
